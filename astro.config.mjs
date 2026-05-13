35aec54b80de6f9546e1fc1b5347d36063194b4e
AlexCastro <castroecija.alejandro@gmail.com>
2026-05-13 19:46:26 +0200

    SEO AUDIT: fix all issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://alexcastrodev.netlify.app',
+  integrations: [sitemap()],
+  redirects: {
+    '/en/servicios': '/en/services',
+  },
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en'],
